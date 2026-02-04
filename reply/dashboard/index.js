f3f36f3fce33de93ecea26c0f6c276e98b78a7ea
Jorge <info.jorgealcala@gmail.com>
2026-02-04 22:31:46 +0100

    Update index.js

--- reply/dashboard/index.js
+++ reply/dashboard/index.js
@@ -1,6 +1,7 @@
 const { DashboardPlugin } = require("strange-sdk");
 
 module.exports = new DashboardPlugin({
+    name: "reply:PLUGIN.NAME",
     icon: "fa-solid fa-reply",
     dependencies: [],
     baseDir: __dirname,
