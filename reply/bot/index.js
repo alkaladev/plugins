ef562b1d881c5a75a991b47748e5087e3b2e1524
Jorge <info.jorgealcala@gmail.com>
2026-02-04 18:56:22 +0100

    changes

--- reply/bot/index.js
+++ reply/bot/index.js
@@ -1,24 +1,6 @@
-const { BotPlugin } = require('strange-sdk');
+const { BotPlugin } = require("strange-sdk");
 
-class ResponderPlugin extends BotPlugin {
-    constructor() {
-        super({
-            name: 'reply',
-            baseDir: __dirname //
-        });
-    }
-
-    async onReady() {
-        this.client.logger.info('Plugin Responder habilitado y listo.');
-    }
-
-    async onMessage(message) {
-        if (message.author.bot) return;
-        
-        if (message.content.toLowerCase() === '!hola') {
-            await message.reply('¡Hola Jorge! El plugin ya reconoce la ruta correctamente. 🚀');
-        }
-    }
-}
-
-module.exports = new ResponderPlugin();
+module.exports = new BotPlugin({
+    dependencies: [],
+    baseDir: __dirname,
+});
--- reply/dashboard/index.js
+++ reply/dashboard/index.js
@@ -1,25 +1,6 @@
-// bot/plugins/reply/index.js (Versión con Listener)
-const { BotPlugin } = require("strange-sdk");
+const { DashboardPlugin } = require("strange-sdk");
 
-module.exports = new BotPlugin({
+module.exports = new DashboardPlugin({
     dependencies: [],
-    ownerOnly: false,
     baseDir: __dirname,
-
-    // Este método lo ejecuta el pluginManager cuando recibe eventos
-    async onPluginMessage(message) {
-        const { eventName, data } = message;
-
-        if (eventName === "REPLY_TO_MESSAGE") {
-            const { channelId, messageId, content } = data.payload;
-            try {
-                const channel = await this.client.channels.fetch(channelId);
-                const targetMsg = await channel.messages.fetch(messageId);
-                await targetMsg.reply(content);
-                return message.reply({ success: true });
-            } catch (error) {
-                return message.reply({ success: false, error: error.message });
-            }
-        }
-    }
 });
