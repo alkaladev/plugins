2bd44d98f8432b7f58f1beb74597f81e72408c83
Jorge <info.jorgealcala@gmail.com>
2026-02-04 18:46:48 +0100

    changes

--- reply/bot/index.js
+++ reply/bot/index.js
@@ -3,18 +3,20 @@ const { BotPlugin } = require('strange-sdk');
 class ResponderPlugin extends BotPlugin {
     constructor() {
         super({
-            name: 'reply'
+            name: 'reply',
+            baseDir: __dirname //
         });
     }
 
     async onReady() {
-        this.client.logger.info('Plugin Responder cargado correctamente.');
+        this.client.logger.info('Plugin Responder habilitado y listo.');
     }
 
     async onMessage(message) {
         if (message.author.bot) return;
+        
         if (message.content.toLowerCase() === '!hola') {
-            await message.reply('¡Hola Jorge! Todo funciona. 🚀');
+            await message.reply('¡Hola Jorge! El plugin ya reconoce la ruta correctamente. 🚀');
         }
     }
 }
