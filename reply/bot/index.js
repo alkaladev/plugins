92c34ea39aa9e9322fcbb6acc29c0d545fb4b8b7
Jorge <info.jorgealcala@gmail.com>
2026-02-04 19:24:12 +0100

    locales fix

--- reply/bot/index.js
+++ reply/bot/index.js
@@ -3,5 +3,4 @@ const { BotPlugin } = require("strange-sdk");
 module.exports = new BotPlugin({
     dependencies: [],
     baseDir: __dirname,
-    localesDir: path.join(__dirname, '..', 'locales')
 });
