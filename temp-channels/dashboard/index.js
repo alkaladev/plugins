2d458a833fd10055053f0d1fd9def1e5285e481d
Jorge <info.jorgealcala@gmail.com>
2026-02-15 21:59:09 +0100

    s

--- temp-channels/dashboard/index.js
+++ temp-channels/dashboard/index.js
@@ -1,31 +1,9 @@
 const { DashboardPlugin } = require("strange-sdk");
-const { Logger } = require("strange-sdk/utils");
-const dbService = require("../db.service");
 
-class TempChannelsDashboardPlugin extends DashboardPlugin {
-    constructor() {
-        super({
-            dependencies: [],
-            baseDir: __dirname,
-            dbService,
-        });
-    }
-
-    async enable(dbClient) {
-        Logger.info("[TempChannels-Dashboard] Plugin habilitado");
-    }
-
-    async disable() {
-        Logger.info("[TempChannels-Dashboard] Plugin deshabilitado");
-    }
-
-    async onGuildEnable(guildId) {
-        Logger.info(`[TempChannels-Dashboard] Habilitado en guild: ${guildId}`);
-    }
-
-    async onGuildDisable(guildId) {
-        Logger.info(`[TempChannels-Dashboard] Deshabilitado en guild: ${guildId}`);
-    }
-}
-
-module.exports = new TempChannelsDashboardPlugin();
+module.exports = new DashboardPlugin({
+    icon: "fa-solid fa-microphone",
+    dependencies: [],
+    baseDir: __dirname,
+    dashboardRouter: require("./router"),
+    dbService: require("../db.service"),
+});
