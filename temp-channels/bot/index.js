175fa5b0c7b91cb1b8959a97da64d3b3ad4c461d
Jorge <info.jorgealcala@gmail.com>
2026-02-15 22:24:15 +0100

    s
    s

--- temp-channels/bot/index.js
+++ temp-channels/bot/index.js
@@ -3,83 +3,94 @@ const { Logger } = require("strange-sdk/utils");
 
 console.log("[TempChannels] Bot plugin cargando...");
 
-module.exports = new BotPlugin({
-    dependencies: [],
-    baseDir: __dirname,
+try {
+    const plugin = new BotPlugin({
+        dependencies: [],
+        baseDir: __dirname,
 
-    onEnable: (client) => {
-        console.log("[TempChannels] onEnable LLAMADO");
-        Logger.info("[TempChannels] Plugin habilitado - onEnable");
-
-        const dbService = require("../db.service");
-        console.log("[TempChannels] dbService cargado");
-
-        // Escuchar cambios en canales de voz
-        client.on("voiceStateUpdate", async (oldState, newState) => {
+        onEnable: (client) => {
             try {
-                const { channel, member, guild } = newState;
+                console.log("[TempChannels] onEnable LLAMADO");
+                Logger.info("[TempChannels] Plugin habilitado - onEnable");
 
-                if (!oldState.channel && channel) {
-                    console.log("[TempChannels] Usuario conectado a canal:", channel.name);
-                    const settings = await dbService.getSettings(guild.id);
-                    const generator = settings.generators.find((g) => g.sourceChannelId === channel.id);
-                    if (!generator) return;
+                const dbService = require("../db.service");
+                console.log("[TempChannels] dbService cargado");
 
-                    const count = await dbService.getActiveChannelCount(channel.id);
-                    const channelNumber = count + 1;
-                    const tempChannelName = `${generator.namePrefix} ${channelNumber}`;
+                // Escuchar cambios en canales de voz
+                client.on("voiceStateUpdate", async (oldState, newState) => {
+                    try {
+                        const { channel, member, guild } = newState;
 
-                    const tempChannel = await guild.channels.create({
-                        name: tempChannelName,
-                        type: 2,
-                        parent: generator.parentCategoryId || channel.parentId,
-                        userLimit: generator.userLimit || 0,
-                    });
+                        if (!oldState.channel && channel) {
+                            console.log("[TempChannels] Usuario conectado a canal:", channel.name);
+                            const settings = await dbService.getSettings(guild.id);
+                            const generator = settings.generators.find((g) => g.sourceChannelId === channel.id);
+                            if (!generator) return;
 
-                    await dbService.addActiveChannel({
-                        channelId: tempChannel.id,
-                        guildId: guild.id,
-                        sourceChannelId: channel.id,
-                        namePrefix: generator.namePrefix,
-                        createdBy: member.id,
-                    });
+                            const count = await dbService.getActiveChannelCount(channel.id);
+                            const channelNumber = count + 1;
+                            const tempChannelName = `${generator.namePrefix} ${channelNumber}`;
 
-                    await member.voice.setChannel(tempChannel);
-                    Logger.info(`[TempChannels] Canal temporal creado: ${tempChannelName}`);
-                }
-                else if (oldState.channel && (!newState.channel || oldState.channel.id !== newState.channel?.id)) {
-                    console.log("[TempChannels] Usuario desconectado de canal:", oldState.channel.name);
-                    const guild = oldState.guild;
-                    if (!guild) return;
+                            const tempChannel = await guild.channels.create({
+                                name: tempChannelName,
+                                type: 2,
+                                parent: generator.parentCategoryId || channel.parentId,
+                                userLimit: generator.userLimit || 0,
+                            });
 
-                    const activeChannels = await dbService.getActiveChannels(guild.id);
-                    for (const activeChannel of activeChannels) {
-                        try {
-                            const channel = guild.channels.cache.get(activeChannel.channelId);
-                            if (!channel) {
-                                await dbService.removeActiveChannel(activeChannel.channelId);
-                                continue;
-                            }
+                            await dbService.addActiveChannel({
+                                channelId: tempChannel.id,
+                                guildId: guild.id,
+                                sourceChannelId: channel.id,
+                                namePrefix: generator.namePrefix,
+                                createdBy: member.id,
+                            });
 
-                            if (channel.members.size === 0) {
-                                await channel.delete();
-                                await dbService.removeActiveChannel(activeChannel.channelId);
-                                Logger.info(`[TempChannels] Canal temporal eliminado: ${channel.name}`);
-                            }
-                        } catch (error) {
-                            Logger.error("[TempChannels] Error limpiando canal:", error);
+                            await member.voice.setChannel(tempChannel);
+                            Logger.info(`[TempChannels] Canal temporal creado: ${tempChannelName}`);
                         }
+                        else if (oldState.channel && (!newState.channel || oldState.channel.id !== newState.channel?.id)) {
+                            console.log("[TempChannels] Usuario desconectado de canal:", oldState.channel.name);
+                            const guild = oldState.guild;
+                            if (!guild) return;
+
+                            const activeChannels = await dbService.getActiveChannels(guild.id);
+                            for (const activeChannel of activeChannels) {
+                                try {
+                                    const channel = guild.channels.cache.get(activeChannel.channelId);
+                                    if (!channel) {
+                                        await dbService.removeActiveChannel(activeChannel.channelId);
+                                        continue;
+                                    }
+
+                                    if (channel.members.size === 0) {
+                                        await channel.delete();
+                                        await dbService.removeActiveChannel(activeChannel.channelId);
+                                        Logger.info(`[TempChannels] Canal temporal eliminado: ${channel.name}`);
+                                    }
+                                } catch (error) {
+                                    Logger.error("[TempChannels] Error limpiando canal:", error);
+                                }
+                            }
+                        }
+                    } catch (error) {
+                        Logger.error("[TempChannels] Error en voiceStateUpdate:", error);
                     }
-                }
+                });
+
+                console.log("[TempChannels] Event listener de voiceStateUpdate registrado");
             } catch (error) {
-                Logger.error("[TempChannels] Error en voiceStateUpdate:", error);
+                console.error("[TempChannels] Error en onEnable:", error);
+                Logger.error("[TempChannels] Error en onEnable:", error);
             }
-        });
+        },
 
-        console.log("[TempChannels] Event listener de voiceStateUpdate registrado");
-    },
+        dbService: require("../db.service"),
+    });
 
-    dbService: require("../db.service"),
-});
-
-console.log("[TempChannels] Bot plugin exportado");
+    module.exports = plugin;
+    console.log("[TempChannels] Bot plugin exportado exitosamente");
+} catch (error) {
+    console.error("[TempChannels] Error exportando bot plugin:", error);
+    throw error;
+}
--- temp-channels/dashboard/index.js
+++ temp-channels/dashboard/index.js
@@ -2,22 +2,30 @@ const { DashboardPlugin } = require("strange-sdk");
 
 console.log("[TempChannels] Dashboard plugin cargando...");
 
-const dashboardRouter = require("./router");
-const adminRouter = require("./admin.router");
-const dbService = require("../db.service");
+try {
+    const dashboardRouter = require("./router");
+    const adminRouter = require("./admin.router");
+    const dbService = require("../db.service");
 
-module.exports = new DashboardPlugin({
-    icon: "fa-solid fa-microphone",
-    baseDir: __dirname,
-    dashboardRouter: (req, res, next) => {
-        console.log("[TempChannels] dashboardRouter middleware llamado");
-        dashboardRouter(req, res, next);
-    },
-    adminRouter: (req, res, next) => {
-        console.log("[TempChannels] adminRouter middleware llamado");
-        adminRouter(req, res, next);
-    },
-    dbService: dbService,
-});
+    console.log("[TempChannels] Routers y dbService cargados");
 
-console.log("[TempChannels] Dashboard plugin exportado");
+    const plugin = new DashboardPlugin({
+        icon: "fa-solid fa-microphone",
+        baseDir: __dirname,
+        dashboardRouter: (req, res, next) => {
+            console.log("[TempChannels] dashboardRouter middleware llamado");
+            dashboardRouter(req, res, next);
+        },
+        adminRouter: (req, res, next) => {
+            console.log("[TempChannels] adminRouter middleware llamado");
+            adminRouter(req, res, next);
+        },
+        dbService: dbService,
+    });
+
+    module.exports = plugin;
+    console.log("[TempChannels] Dashboard plugin exportado exitosamente");
+} catch (error) {
+    console.error("[TempChannels] Error en dashboard plugin:", error);
+    throw error;
+}
