4c12626f862ce8ed0cba475a4cec0a1be8498c1f
Jorge <info.jorgealcala@gmail.com>
2026-02-15 22:20:48 +0100

    Update index.js
    s

--- temp-channels/dashboard/index.js
+++ temp-channels/dashboard/index.js
@@ -2,12 +2,22 @@ const { DashboardPlugin } = require("strange-sdk");
 
 console.log("[TempChannels] Dashboard plugin cargando...");
 
+const dashboardRouter = require("./router");
+const adminRouter = require("./admin.router");
+const dbService = require("../db.service");
+
 module.exports = new DashboardPlugin({
     icon: "fa-solid fa-microphone",
     baseDir: __dirname,
-    dashboardRouter: require("./router"),
-    adminRouter: require("./admin.router"),
-    dbService: require("../db.service"),
+    dashboardRouter: (req, res, next) => {
+        console.log("[TempChannels] dashboardRouter middleware llamado");
+        dashboardRouter(req, res, next);
+    },
+    adminRouter: (req, res, next) => {
+        console.log("[TempChannels] adminRouter middleware llamado");
+        adminRouter(req, res, next);
+    },
+    dbService: dbService,
 });
 
 console.log("[TempChannels] Dashboard plugin exportado");
